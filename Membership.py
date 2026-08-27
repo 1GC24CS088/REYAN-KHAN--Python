e152cc0bd92d25859bf9d576b4485fd66c4db215
MD.MAAZ <mazz@gmail.com>
2026-08-27 16:24:39 +0530

    commit Initial

---
+++ Membership.py
@@ -0,0 +1,7 @@
+student = {
+    "name": "Reyan",
+    "age": 20
+}
+
+print("name" in student)
+print("marks" in student)
---
+++ if__name_ = main.py
@@ -0,0 +1,12 @@
+# if __name__ == "__main__"
+def mul(x, y):
+    return x * y
+
+def main():
+    a = 15
+    b = 30
+    result = mul(a, b)
+    print("Sum =", result)
+
+if __name__ == "__main__":
+    main()
