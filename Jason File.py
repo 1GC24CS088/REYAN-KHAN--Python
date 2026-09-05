f833d87613847f43993dce545d2b56ffe7ad59c5
MD.MAAZ <mazz@gmail.com>
2026-09-05 19:32:40 +0530

    Commit

---
+++ Jason File.py
@@ -0,0 +1,19 @@
+# Jason File
+# Python writing files (.txt, .json, .csv)
+
+import json
+
+employee = {
+    "name": "Sam",
+    "age": 30,
+    "job": "cook"
+}
+
+file_path = "C:/Users/ADMIN/Desktop/output.jason"
+
+try:
+    with open(file_path, "w") as file:
+        json.dump(employee, file, indent=4)
+        print(f"json file '{file_path}' was created")
+except FileExistsError:
+    print("That file already exists!")
